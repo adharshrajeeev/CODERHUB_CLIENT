3cc4dd7aa98c219cf0462f01c76dac1fbcde4ab2
Adharsh R <adharshrajeeev2000@gmail.com>
2023-05-16 20:39:33 +0530

    changed block user tab,added border in msg,added min age in reg,changed localhost to http

--- src/utils/Services.js
+++ src/utils/Services.js
@@ -5,12 +5,12 @@ import jwtDecode from "jwt-decode";
 export default function decodeToken(){
     try{
         const token=localStorage.getItem('token');
-        if(!token) return window.location.href('http://localhost:3000');
+        if(!token) return window.location.href('https://www.coderhub.cloud');
         const {id}=jwtDecode(token);
         return id
         
     }catch(err){
-        return  window.location.href('http://localhost:3000');
+        return  window.location.href('https://www.coderhub.cloud');
     }
 }
 
